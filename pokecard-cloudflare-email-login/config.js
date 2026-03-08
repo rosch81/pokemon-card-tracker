d995e2953af3b6ab202bfb03168c59ecc0c2d2bb
rosch81 <roschcs@gmail.com>
2026-03-08 15:32:54 -0500

    Update config.js

--- pokecard-cloudflare-email-login/config.js
+++ pokecard-cloudflare-email-login/config.js
@@ -1,4 +1,4 @@
 export const APP_CONFIG = {
-  SUPABASE_URL: https://bhgndygxyvpmfrxhjbgm.supabase.co",
+  SUPABASE_URL: "https://bhgndygxyvpmfrxhjbgm.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_1M6rePP2ys8-0Gusx-qL5Q_-mPmFHxn"
 };
